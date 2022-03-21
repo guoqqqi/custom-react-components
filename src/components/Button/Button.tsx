77a6ab995ad2bdb5a4328747d9e0fa96d171aa8c
guoqqqi <979918879@qq.com>
2022-03-21 16:47:52 +0800

    feat: add default button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,18 @@ import "./Button.css";
 
 export interface ButtonProps {
   label: string;
+  style?: React.CSSProperties | undefined;
+  type?: 'default' | 'danger' | 'primary';
+  onClick?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button
+      onClick={props.onClick}
+      style={props.style}
+    >
+      {props.label}
+  </button>;
 };
 
 export default Button;
