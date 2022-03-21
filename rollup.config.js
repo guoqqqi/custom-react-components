3a9fa98d388fa806422656fd2c451fb30f682f33
guoqqqi <979918879@qq.com>
2022-03-21 14:44:30 +0800

    fix: update dependencies and version

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
